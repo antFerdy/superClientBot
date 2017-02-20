68cd2ef750b15fc5bb27eacca4366fb972c89787
Rustem <Rustem@MacBook-Air-ww.local>
2017-02-21 01:23:34 +0600

    fix controller

--- app/controllers/BotController.java
+++ app/controllers/BotController.java
@@ -8,17 +8,12 @@ public class BotController extends Controller{
 	
 	
 	public Result getMain() {
-		String reqText = request().body().asJson().textValue();
-		Logger.info(reqText);
+		
 		return ok("Hello guys! I am superClient Bot");
 	}
 
 	public Result updateOccured() {
-		String reqText = request().body().asJson().textValue();
-		if(!reqText.isEmpty()) {
-			System.out.println(reqText);
-		}
-		
+		String reqText = request().body().asText();
 		Logger.info(reqText);
 		Logger.info("hello world");
 		return ok();
