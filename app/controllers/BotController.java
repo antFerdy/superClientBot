61eb31537060b20a8ef72ebd5ac2ea33c2922807
Rustem <Rustem@MacBook-Air-ww.local>
2017-02-28 01:15:28 +0600

    checked telegram

--- app/controllers/BotController.java
+++ app/controllers/BotController.java
@@ -1,8 +1,11 @@
 package controllers;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
+import com.fasterxml.jackson.databind.JsonNode;
 
 import play.Logger;
 import play.mvc.Controller;
@@ -14,45 +17,13 @@ public class BotController extends Controller{
 		return ok("Hello guys! I am superClient Bot");
 	}
 
-	public Result updateOccured() {
-//		String reqText = request().getHeader(CONTENT_TYPE);
-//		Logger.info("Content type: ");
-//		Logger.info(reqText);
+	public Result updateOccured() {		
+		JsonNode json = request().body().asJson();
+		Iterator<JsonNode> it = json.elements();
 		
-		Map<String, String[]> headers = request().headers();
-		Set<String> keys = headers.keySet();
-		
-		for(String key : keys) {
-			Logger.info("The key is: " + key);
-			String[] values = headers.get(key);
-			for(String value : values) {
-				Logger.info("The Values of the key: " + value);
-			}
+		while(it.hasNext()) {
+			Logger.info(it.next().textValue());
 		}
-		
-//		RequestBody body = request().body();
-//		Logger.info("Size of body as json: " + body.asJson().size());
-		
-		play.mvc.Http.MultipartFormData form = request().body().asMultipartFormData();
-		List files = form.getFiles();
-		Logger.info("Size of multipart form: " + files.size());
-		
-		
-		
-		
-		
-//		Map<String, String[]> mapForm = request().body().asFormUrlEncoded();
-//		if(mapForm != null || !mapForm.equals(null))
-//			Logger.info("Size of map form " + mapForm.size());
-		
-//		Set<String> keysForm = mapForm.keySet();
-//		for(String key : keysForm) {
-//			Logger.info("The key in FORM is: " + key);
-//			String[] values = mapForm.get(key);
-//			for(String value : values) {
-//				Logger.info("The Values of the form key: " + value);
-//			}
-//		}
 		
 		return ok("Privet");
 	}
