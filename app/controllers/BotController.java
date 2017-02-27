9fa3936242bc4727e7961df08f5e96bf608f4599
Rustem <Rustem@MacBook-Air-ww.local>
2017-02-27 22:48:32 +0600

    Changes in controller

--- app/controllers/BotController.java
+++ app/controllers/BotController.java
@@ -1,19 +1,22 @@
 package controllers;
 
+import java.util.concurrent.CompletionStage;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.inject.Inject;
+
 import play.Logger;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 public class BotController extends Controller{
 	
-	
 	public Result getMain() {
-		
 		return ok("Hello guys! I am superClient Bot");
 	}
 
 	public Result updateOccured() {
-		String reqText = request().body().asText();
+		String reqText = request().body().asRaw().toString();
 		Logger.info(reqText);
 		Logger.info("hello world");
 		return ok();
