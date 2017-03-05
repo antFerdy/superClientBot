3f10b181dcb4d10b918f872c2757826a3bcce2b4
Rustem <Rustem@MacBook-Air-ww.local>
2017-03-06 00:06:09 +0600

    Add second reply feature

--- app/utils/UpdateHandler.java
+++ app/utils/UpdateHandler.java
@@ -1,5 +1,8 @@
 package utils;
 
+import java.util.Date;
+import java.util.concurrent.CompletionStage;
+
 import javax.inject.Inject;
 import play.libs.ws.*;
 import models.Message;
@@ -9,9 +12,14 @@ public class UpdateHandler {
 
 	private static final String url = "https://api.telegram.org/bot283733008:AAGYER7EsbD0ESpkJ3tsaBJgvAet6sg8UiI/sendMessage";
 	private WSClient ws;
+	private long chatId = 0L;
+	private long firstMsgTime;
+	
 	
 	private static final String firtReply = "Привет! Я робот, собирающий отзывы о компаниях. Делаю Клиентов и компании ближе друг к другу. "
 			+ "\nОтзыв о какой компании вы хотите оставить (название компании)?";
+	private static final String secondReply = "В каком городе находится данная компания?";
+	
 	
 	public UpdateHandler(WSClient ws) {
 		// TODO Auto-generated constructor stub
@@ -20,9 +28,25 @@ public class UpdateHandler {
 
 	public void handle(Update u) {
 		String msgTxt = u.getMessage().getText();
-		long chatId = u.getMessage().getChat().getId();
+		if(chatId != 0L) {
+			chatId = u.getMessage().getChat().getId();
+			firstMsgTime = u.getMessage().getDate();
+		}
+		
 		if(msgTxt.equals("/start")) {
 			sendMessage(chatId, firtReply);
+		} else {
+			long id = u.getMessage().getChat().getId();
+			long newTime = u.getMessage().getDate();
+			if(id == chatId) {
+				System.out.println(new Date(newTime));
+				System.out.println(new Date(firstMsgTime));
+				System.out.println(msgTxt);
+				
+				if(newTime - firstMsgTime < 36000L) {
+					sendMessage(chatId, secondReply);
+				}
+			}
 		}
 		
 		
@@ -34,7 +58,8 @@ public class UpdateHandler {
 		WSRequest request = ws.url(url);
 		request.setQueryParameter("chat_id", String.valueOf(chat_id));
 		request.setQueryParameter("text", text);
-		request.get();
+		CompletionStage<WSResponse> rs = request.get();
+		
 	}
 
 }
